ef8b67b7bb613ab1e5441da1b6ce01a0ca458e35
Michael Hermansen <michael.p.hermansen@gmail.com>
2021-08-22 15:57:37 +0200

    kontakt-side

--- src/pages/kontakt.tsx
+++ src/pages/kontakt.tsx
@@ -1,3 +1,4 @@
+import classes from "@modules/Contact.module.scss";
 import Container from "@components/Container";
 import Layout from "@components/Layout";
 
@@ -5,7 +6,18 @@ const Contact = () => {
 	return (
 		<Layout title="Kontakt" description="…">
 			<Container>
-				<h1>Kontakt</h1>
+				<h1 className={classes.page_title}>Kontakt</h1>
+				<ul className={classes.contact_info_list}>
+					<li className={classes.contact_info_item}>
+						E-post:{" "}
+						<a href="mailto:michael.p.hermansen@gmail.com">
+							michael.p.hermansen@gmail.com
+						</a>
+					</li>
+					<li className={classes.contact_info_item}>
+						Mobil: <a href="tel:+47 406 10 143">406 10 143</a>
+					</li>
+				</ul>
 			</Container>
 		</Layout>
 	);
