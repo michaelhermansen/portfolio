0b1ac5e117fc063d5346ba576f0f3ac8085c1751
Michael Hermansen <michael.p.hermansen@gmail.com>
2021-08-22 17:01:27 +0200

    ..

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,7 @@
 import "../styles/global.scss";
-import Head from "next/head";
 
 const MyApp = ({ Component, pageProps }) => {
-	return (
-		<>
-			<Component {...pageProps} />
-		</>
-	);
+	return <Component {...pageProps} />;
 };
 
 export default MyApp;
