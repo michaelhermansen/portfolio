b760970a802ecb3b578ef83e9fea1b77f5f8ee35
Michael Hermansen <michael.p.hermansen@gmail.com>
2021-08-25 13:40:50 +0200

    lagt til polyfill for scrollbehavior: 'smooth'

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "../styles/global.scss";
+import smoothscroll from "smoothscroll-polyfill";
+import { useEffect } from "react";
 
 const MyApp = ({ Component, pageProps }) => {
+	useEffect(() => {
+		smoothscroll.polyfill();
+	}, []);
+
 	return <Component {...pageProps} />;
 };
 
