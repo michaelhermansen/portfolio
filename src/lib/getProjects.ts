7cbbfa143c5e40f414b788282369a9fd49c77db7
Michael Hermansen <9354726+michaelhermansen@users.noreply.github.com>
2022-01-05 22:03:28 +0100

    Added .env.example, added error handeling for missing variables

--- src/lib/getProjects.ts
+++ src/lib/getProjects.ts
@@ -5,16 +5,23 @@ const SPACE_ID = process.env.CONTENTFUL_SPACE_ID;
 const ACCESS_TOKEN = process.env.CONTENTFUL_ACCESS_TOKEN;
 
 const getProjects = async (slug?: string): Promise<IProjectFields[]> => {
+	// Handle missing environment variables.
+	if (!SPACE_ID || !ACCESS_TOKEN) {
+		console.error("Contentful space-ID or access token is missing.");
+		return [];
+	}
+
 	const client = createClient({
 		space: SPACE_ID,
 		accessToken: ACCESS_TOKEN,
 	});
 
-	const entries: EntryCollection<IProjectFields> =
-		await client.getEntries<IProjectFields>({
-			content_type: "project",
-			"fields.slug": slug,
-		});
+	const entries: EntryCollection<IProjectFields> = await client.getEntries<
+		IProjectFields
+	>({
+		content_type: "project",
+		"fields.slug": slug,
+	});
 
 	const projects = entries.items.map(item => item.fields);
 	return projects;
